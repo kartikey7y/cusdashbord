250afcd507ef481ae31203ab8a8d5b2b97b8d842
kartikeyevanik <kartikey.evanik@gmail.com>
2025-09-01 13:34:21 +0530

    v1.0.3

--- components/widgets/randomImg.tsx
+++ components/widgets/randomImg.tsx
@@ -1,26 +1,37 @@
 "use client"
 
 import axios from "axios"
+import { error } from "console"
 import Image from "next/image"
 import { useEffect, useState } from "react"
 
 export default function RandomImage() {
     const [image, setImage] = useState<string | null>(null)
+    const [error, setError] = useState<boolean>(false)
 
     useEffect(() => {
-        axios.get("https://dog.ceo/api/breeds/image/random")
-            .then(res => setImage(res.data.message))
-            .catch(err => console.error(err))
+        try {
+            axios.get("https://dog.ceo/api/breeds/image/random")
+                .then(res => setImage(res.data.message))
+                .catch(err => console.error(err))
+        } catch (e) {
+            setError(true)
+            console.log(error)
+        }
+
     }, [])
 
     if (!image) {
         return <p>Loading...</p>
     }
+    if (error) {
+        return <p>Error in fetching data </p>
+    }
 
     return (
         <div className="flex flex-col items-center justify-center p-4">
-                <h1 className="font-bold">Image</h1>
-              
+            <h1 className="font-bold">Image</h1>
+
             <Image
                 src={image}
                 alt="Random dog"
